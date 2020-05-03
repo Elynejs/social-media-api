aef4064137c20e560e95d755d95912b5d1c44799
Elynejs <matthias.gomes03@gmail.com>
2020-05-04 00:26:49 +0200

    update twitter
    changed the script to find and download every picture in iterated tweets

--- twitter.py
+++ twitter.py
@@ -1,21 +1,31 @@
 from twitter_scraper import *
+import urllib.request
 import seaborn as sb
+import random
 
 trends = get_trends()
 data = dict()
 print(trends)
 
+def dl_img(url, name):
+    path = f'./images/{name}.jpg'
+    urllib.request.urlretrieve(url, path)
+
+
 i = 0
 while i < len(trends):
-    k = 0
-    try:
-        for tweet in get_tweets(trends[i], pages=1):
-            print(tweet['tweetId'])
-            k += 1
-            data[trends[i]] = k
-            print(data)
-    except:
-        print(data)
-        print('error occured')
-        break
+    for tweet in get_tweets(trends[i], pages=1):
+        if len(tweet['entries']['photos']):
+            j = 0
+            while j < len(tweet['entries']['photos']):
+                url = tweet['entries']['photos'][j]
+                try:
+                    t_id = tweet['tweetId']
+                    randint = random.randint(0, 9999)
+                    name = f'{t_id}{randint}'
+                    dl_img(url, name)
+                    print('image downloaded')
+                except:
+                    print('couldn\'t download image')
+                j += 1
     i += 1
