ba600f56f6adafb0246dd320af91c4996f988cc4
Elynejs <matthias.gomes03@gmail.com>
2020-05-05 03:45:57 +0200

    improved the spaghetti

--- twitter.py
+++ twitter.py
@@ -4,55 +4,33 @@ import random
 
 trends = get_trends()
 pages_to_search = 10
-urls = []
+ids = []
 
 def dl_img(url, name):
     path = f'./images/{name}.jpg'
     urllib.request.urlretrieve(url, path)
 
-def check_url(url):
-    with open('twitter_img_url.txt', 'r+') as f:
-        if url in f.read():
-            f.close()
-            return False
-        else:
-            f.close()
-            return True
 
-def append_url():
-    with open('twitter_img_url.txt', 'w+') as f:
-        x = 0
-        while x < len(urls):
-            f.write(f'{urls[i]}\n')
-            x += 1
-
-try:
-    for m in range(0, len(trends)):
+for trend in trends:
+    try:
         for i in range(1, pages_to_search):
-            for tweet in get_tweets(trends[m], pages=i):
+            for tweet in get_tweets(trend, pages=i):
                 if len(tweet['entries']['photos']):
-                    for j in range(0, len(tweet['entries']['photos'])):
-                        url = tweet['entries']['photos'][j]
-                        urls.append(url)
-                        if check_url(url):
+                    t_id = tweet['tweetId']
+                    if t_id not in ids:
+                        ids.append(t_id)
+                        for j in range(0, len(tweet['entries']['photos'])):
                             try:
-                                t_id = tweet['tweetId']
-                                if len(tweet['entries']['photos']):
-                                    for l in range(0, len(tweet['entries']['photos'])):
-                                        photo_number = l
-                                        name = f'{t_id}_{photo_number}'
-                                        dl_img(url, name)
-                                        print('image downloaded')
-                                else:
-                                    name = f'{t_id}_1'
-                                    dl_img(url, name)
-                                    print('image downloaded')
+                                url = tweet['entries']['photos'][j]
+                                name = f'{t_id}_{j}'
+                                dl_img(url, name)
+                                print('image downloaded')
                             except:
                                 print('couldn\'t download image')
-                        else:
-                            print('image already downloaded')
+                    else:
+                        print('image already downloaded')
                 else:
                     print('no image found')
-except:
-    print('error occured')
-append_url()
+    except:
+        trends.remove(trend)
+        print(f'remove {trend} from trends because it caused an error')
